119dbe3020a86bd55f61b7d8bb5cebe60652926e
nekomata <73189703+1nekomata@users.noreply.github.com>
2022-01-26 20:47:12 +0100

    Change clang-aosp directories to aosp-clang

--- setup-toolchains.sh
+++ setup-toolchains.sh
@@ -5,9 +5,9 @@ echo "clone aarch64 and arm gcc"
 git clone https://gitlab.com/UBERTC/arm-eabi-7.0 --depth=1
 git clone https://gitlab.com/UBERTC/aarch64-linux-android-7.0-kernel --depth=1 aarch64-linux-android-7.0
 echo "clone AOSP clang"
-git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 --depth=1 clang-aosp
+git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 --depth=1 aosp-clang
 echo "add toolchains to $PATH"
-export PATH=$PATH:$HOME/toolchains/arm-eabi-7.0/bin:$HOME/toolchains/aarch64-linux-android-7.0/bin:$HOME/toolchains/clang-aosp/clang-r437112b/bin
+export PATH=$PATH:$HOME/toolchains/arm-eabi-7.0/bin:$HOME/toolchains/aarch64-linux-android-7.0/bin:$HOME/toolchains/aosp-clang/clang-r437112b/bin
 echo "WARNING you might need to adjust version of clang that you want to use. the repository includes several versions that are stored in separate directories"
 echo "currently version clang-r437112b is used"
 echo " "
@@ -16,7 +16,7 @@ arm-eabi-gcc -v
 aarch64-linux-android-gcc -v
 echo "NOTE temporarily removing every other entry besides gcc and clang toolchains from PATH"
 export PATHBAK=$PATH
-export PATH=$HOME/toolchains/arm-eabi-7.0/bin:$HOME/toolchains/aarch64-linux-android-7.0/bin:$HOME/toolchains/clang-aosp/clang-r437112b/bin
+export PATH=$HOME/toolchains/arm-eabi-7.0/bin:$HOME/toolchains/aarch64-linux-android-7.0/bin:$HOME/toolchains/aosp-clang/clang-r437112b/bin
 clang -v
 echo "restoring original PATH entries"
 export PATHCC=$PATH
@@ -24,7 +24,7 @@ export PATH=$PATHBAK
 echo "if you want to remove all the entries again use export PATH=$PATHCC and to restore it use PATH=$PATHBAK. i recommend doing that or running build.sh if you have another clang version in your PATH"
 echo "adding PATHCC and PATHBAK to .bashrc"
 cat << EOF >> ~/.bashrc
-export PATCC=$HOME/toolchains/arm-eabi-7.0/bin:$HOME/toolchains/aarch64-linux-android-7.0/bin:$HOME/toolchains/clang-aosp/clang-r437112b/bin
+export PATCC=$HOME/toolchains/arm-eabi-7.0/bin:$HOME/toolchains/aarch64-linux-android-7.0/bin:$HOME/toolchains/aosp-clang/clang-r437112b/bin
 export PATHBAK=$PATH
 EOF
 echo "setup complete"
